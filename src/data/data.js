8238c10d71b2bfcb22b22b97d7c094b2c86a166d
Natchel <LebeaN0101@gmail.com>
2024-01-21 02:32:56 +0200

    Project Added

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.png'
+import TacImg from '../assets/projects/tacImg.jpeg'
 
 export const data=[
     {
@@ -16,6 +17,12 @@ export const data=[
         github:"https://github.com/NatchelLebea/TODO-list",
         live:"https://natchellebea.github.io/TODO-list/",
     },
-   
+    {
+        id:3,
+        name:"TicTacToe",
+        image:TacImg,
+        github:"https://github.com/NatchelLebea/tictactoe",
+        live:"https://natchellebea.github.io/tictactoe/",
+    },
 
 ]
